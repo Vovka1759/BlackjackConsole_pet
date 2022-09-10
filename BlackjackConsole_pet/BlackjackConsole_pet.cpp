e5fab3b5e3a2ae6d3e795a56c31c2cc4907b06b9
Vovka1759 <vovka1759@gmail.com>
2022-09-10 21:14:57 +0300

    Add project files.

---
+++ BlackjackConsole_pet/BlackjackConsole_pet.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <random>
+int getRandomCard() {
+
+    std::random_device rd; 
+    std::mt19937 gen(rd()); 
+    std::uniform_int_distribution<> distr(0, 11); 
+    return distr(gen);
+}
+
+int cardToPoint(char card) {
+    if ((int)(card - '0') < 10) {
+        return card - '0';
+    }
+    else if (card == 'J' or card == 'Q' or card == 'K') {
+        return 10;
+    }
+    else {
+        return 11;
+    }
+}
+
+int main()
+{   
+    char cards[12] = { '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A'};
+    while (true)
+    {
+        int num = getRandomCard();
+        std::cout << cards[num]<< " " << cardToPoint(cards[num]);
+        std::cin.get();
+    }
+    
+}
+
